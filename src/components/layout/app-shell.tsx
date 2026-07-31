33090f3273536a5aa43f22c74e51680ab2940cb3
Nhatchymt3 <nhatchymte@gmail.com>
2026-07-31 08:59:23 +0700

    fix: disable main content body vertical scrollbar

--- src/components/layout/app-shell.tsx
+++ src/components/layout/app-shell.tsx
@@ -168,7 +168,7 @@ export function AppShell({ children, showLeaderboard, setShowLeaderboard }: AppS
       </aside>
 
       {/* Main Content Area */}
-      <main className="flex-1 min-h-0 relative overflow-y-auto">
+      <main className="flex-1 min-h-0 relative overflow-hidden">
         {children}
       </main>
 
